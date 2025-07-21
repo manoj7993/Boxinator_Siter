a8af2d7e7c1227efc0e1c3ab52639fb6ba88e61a
Reddy Manoj Jalla <reddymanojjalla@gmail.com>
2025-07-21 12:06:14 +0530

    Uncomment model imports and associations in index.js

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,38 +1,38 @@
 const { sequelize } = require('../config/database');
 
 // Import all models here
-// const User = require('./User');
-// const UserProfile = require('./UserProfile');
-// const Shipment = require('./Shipment');
-// const Country = require('./Country');
-// const WeightTier = require('./WeightTier');
-// const ShipmentStatus = require('./ShipmentStatus');
-// const ShipmentStatusHistory = require('./ShipmentStatusHistory');
-// const EmailVerificationToken = require('./EmailVerificationToken');
-// const PasswordResetToken = require('./PasswordResetToken');
-// const RefreshToken = require('./RefreshToken');
-// const AdminActionLog = require('./AdminActionLog');
-// const CostAudit = require('./CostAudit');
-// const CountryMultiplierLog = require('./CountryMultiplierLog');
+const User = require('./User');
+const UserProfile = require('./UserProfile');
+const Shipment = require('./Shipment');
+const Country = require('./Country');
+const WeightTier = require('./WeightTier');
+const ShipmentStatus = require('./ShipmentStatus');
+const ShipmentStatusHistory = require('./ShipmentStatusHistory');
+const EmailVerificationToken = require('./EmailVerificationToken');
+const PasswordResetToken = require('./PasswordResetToken');
+const RefreshToken = require('./RefreshToken');
+const AdminActionLog = require('./AdminActionLog');
+const CostAudit = require('./CostAudit');
+const CountryMultiplierLog = require('./CountryMultiplierLog');
 
 // Define associations here
-// User.hasOne(UserProfile, { foreignKey: 'userId' });
-// UserProfile.belongsTo(User, { foreignKey: 'userId' });
+User.hasOne(UserProfile, { foreignKey: 'userId' });
+UserProfile.belongsTo(User, { foreignKey: 'userId' });
 
 // Export all models
 module.exports = {
   sequelize,
-  // User,
-  // UserProfile,
-  // Shipment,
-  // Country,
-  // WeightTier,
-  // ShipmentStatus,
-  // ShipmentStatusHistory,
-  // EmailVerificationToken,
-  // PasswordResetToken,
-  // RefreshToken,
-  // AdminActionLog,
-  // CostAudit,
-  // CountryMultiplierLog,
+  User,
+  UserProfile,
+  Shipment,
+  Country,
+  WeightTier,
+  ShipmentStatus,
+  ShipmentStatusHistory,
+  EmailVerificationToken,
+  PasswordResetToken,
+  RefreshToken,
+  AdminActionLog,
+  CostAudit,
+  CountryMultiplierLog,
 };
